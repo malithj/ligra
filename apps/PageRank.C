a8eb8339249d200692b566d4db00f659f111e909
Malith Jayaweera <malithjayaweera.d@northeastern.edu>
2021-12-11 13:34:37 -0500

    added method to write output to file

--- apps/PageRank.C
+++ apps/PageRank.C
@@ -1,5 +1,5 @@
 // This code is part of the project "Ligra: A Lightweight Graph Processing
-// Framework for Shared Memory", presented at Principles and Practice of 
+// Framework for Shared Memory", presented at Principles and Practice of
 // Parallel Programming, 2013.
 // Copyright (c) 2013 Julian Shun and Guy Blelloch
 //
@@ -26,41 +26,44 @@
 
 template <class vertex>
 struct PR_F {
-  double* p_curr, *p_next;
+  double *p_curr, *p_next;
   vertex* V;
-  PR_F(double* _p_curr, double* _p_next, vertex* _V) : 
-    p_curr(_p_curr), p_next(_p_next), V(_V) {}
-  inline bool update(uintE s, uintE d){ //update function applies PageRank equation
-    p_next[d] += p_curr[s]/V[s].getOutDegree();
+  PR_F(double* _p_curr, double* _p_next, vertex* _V)
+      : p_curr(_p_curr), p_next(_p_next), V(_V) {}
+  inline bool update(uintE s,
+                     uintE d) {  // update function applies PageRank equation
+    p_next[d] += p_curr[s] / V[s].getOutDegree();
     return 1;
   }
-  inline bool updateAtomic (uintE s, uintE d) { //atomic Update
-    writeAdd(&p_next[d],p_curr[s]/V[s].getOutDegree());
+  inline bool updateAtomic(uintE s, uintE d) {  // atomic Update
+    writeAdd(&p_next[d], p_curr[s] / V[s].getOutDegree());
     return 1;
   }
-  inline bool cond (intT d) { return cond_true(d); }};
+  inline bool cond(intT d) { return cond_true(d); }
+};
 
-//vertex map function to update its p value according to PageRank equation
+// vertex map function to update its p value according to PageRank equation
 struct PR_Vertex_F {
   double damping;
   double addedConstant;
   double* p_curr;
   double* p_next;
-  PR_Vertex_F(double* _p_curr, double* _p_next, double _damping, intE n) :
-    p_curr(_p_curr), p_next(_p_next), 
-    damping(_damping), addedConstant((1-_damping)*(1/(double)n)){}
-  inline bool operator () (uintE i) {
-    p_next[i] = damping*p_next[i] + addedConstant;
+  PR_Vertex_F(double* _p_curr, double* _p_next, double _damping, intE n)
+      : p_curr(_p_curr),
+        p_next(_p_next),
+        damping(_damping),
+        addedConstant((1 - _damping) * (1 / (double)n)) {}
+  inline bool operator()(uintE i) {
+    p_next[i] = damping * p_next[i] + addedConstant;
     return 1;
   }
 };
 
-//resets p
+// resets p
 struct PR_Vertex_Reset {
   double* p_curr;
-  PR_Vertex_Reset(double* _p_curr) :
-    p_curr(_p_curr) {}
-  inline bool operator () (uintE i) {
+  PR_Vertex_Reset(double* _p_curr) : p_curr(_p_curr) {}
+  inline bool operator()(uintE i) {
     p_curr[i] = 0.0;
     return 1;
   }
@@ -68,33 +71,69 @@ struct PR_Vertex_Reset {
 
 template <class vertex>
 void Compute(graph<vertex>& GA, commandLine P) {
-  long maxIters = P.getOptionLongValue("-maxiters",100);
+  long maxIters = P.getOptionLongValue("-maxiters", 100);
   const intE n = GA.n;
   const double damping = 0.85, epsilon = 0.0000001;
-  
-  double one_over_n = 1/(double)n;
-  double* p_curr = newA(double,n);
-  {parallel_for(long i=0;i<n;i++) p_curr[i] = one_over_n;}
-  double* p_next = newA(double,n);
-  {parallel_for(long i=0;i<n;i++) p_next[i] = 0;} //0 if unchanged
-  bool* frontier = newA(bool,n);
-  {parallel_for(long i=0;i<n;i++) frontier[i] = 1;}
 
-  vertexSubset Frontier(n,n,frontier);
-  
+  double one_over_n = 1 / (double)n;
+  double* p_curr = newA(double, n);
+  { parallel_for(long i = 0; i < n; i++) p_curr[i] = one_over_n; }
+  double* p_next = newA(double, n);
+  { parallel_for(long i = 0; i < n; i++) p_next[i] = 0; }  // 0 if unchanged
+  bool* frontier = newA(bool, n);
+  { parallel_for(long i = 0; i < n; i++) frontier[i] = 1; }
+
+  vertexSubset Frontier(n, n, frontier);
+
   long iter = 0;
-  while(iter++ < maxIters) {
-    edgeMap(GA,Frontier,PR_F<vertex>(p_curr,p_next,GA.V),0, no_output);
-    vertexMap(Frontier,PR_Vertex_F(p_curr,p_next,damping,n));
-    //compute L1-norm between p_curr and p_next
-    {parallel_for(long i=0;i<n;i++) {
-      p_curr[i] = fabs(p_curr[i]-p_next[i]);
-      }}
-    double L1_norm = sequence::plusReduce(p_curr,n);
-    if(L1_norm < epsilon) break;
-    //reset p_curr
-    vertexMap(Frontier,PR_Vertex_Reset(p_curr));
-    swap(p_curr,p_next);
+  while (iter++ < maxIters) {
+    edgeMap(GA, Frontier, PR_F<vertex>(p_curr, p_next, GA.V), 0, no_output);
+    vertexMap(Frontier, PR_Vertex_F(p_curr, p_next, damping, n));
+    // compute L1-norm between p_curr and p_next
+    {
+      parallel_for(long i = 0; i < n; i++) {
+        p_curr[i] = fabs(p_curr[i] - p_next[i]);
+      }
+    }
+    double L1_norm = sequence::plusReduce(p_curr, n);
+    if (L1_norm < epsilon) break;
+    // reset p_curr
+    vertexMap(Frontier, PR_Vertex_Reset(p_curr));
+    swap(p_curr, p_next);
   }
-  Frontier.del(); free(p_curr); free(p_next); 
+
+  char* oFileName = P.getArgument(0);
+  char* outputDirName = P.getOptionValue("-o");
+
+  std::unique_ptr<char[]> oFileNameCpy =
+      std::make_unique<char[]>(std::strlen(oFileName) + 1);
+  strcpy(oFileNameCpy.get(), oFileName);
+
+  std::string prefix("res_");
+  std::string outputDir(outputDirName);
+
+  if (outputDir[outputDir.length() - 1] != '/') {
+    outputDir.append("/");
+  }
+
+  if (oFileName != nullptr) {
+    const char* delimiters = "/";
+    char* token = std::strtok(oFileNameCpy.get(), delimiters);
+    std::string oFileStr;
+    while (token) {
+      oFileStr = oFileStr.assign(token);
+      token = std::strtok(nullptr, delimiters);
+    }
+    oFileStr = outputDir.append(prefix.append(oFileStr));
+    ofstream outFile;
+    outFile.open(oFileStr);
+    for (uint64_t i = 0; i < n; ++i) {
+      outFile << i << " " << p_next[i] << '\n';
+    }
+    outFile.close();
+  }
+
+  Frontier.del();
+  free(p_curr);
+  free(p_next);
 }
